1e677d4b6cce4104a88e9b456b70768e32ca17de
LirakXhelili <121616011+LirakXhelili@users.noreply.github.com>
2025-04-02 19:21:18 +0200

    Instance Data Class

---
+++ supply.py
@@ -0,0 +1,11 @@
+class supply:
+
+    warehouse_id = 0
+    store_id = 0
+    cost = 0
+
+    def __init__(self,warehouse_id,store_id,cost):
+        self.warehouse_id = warehouse_id
+        self.store_id = store_id
+        self.cost = cost
+
