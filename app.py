b25a99d5b9b7501f0f8d5f374a8573372311006b
LirakXhelili <121616011+LirakXhelili@users.noreply.github.com>
2025-04-16 19:49:39 +0200

    Lirak

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from models.parser import WarehouseParser, write_results_to_file
+from models.parser import WarehouseParser
 from models.solution import InitialSolution
 
 if __name__ == '__main__':
-    input_file = './inputs/wlp14.dzn'  # Adjust the file path as needed
+    input_file = './inputs/wlp01.dzn'  # Adjust the file path as needed
     output_file = './Output/output.txt'  # The output file path
 
     # Initialize and parse the data
     parser = WarehouseParser(input_file)
     data = parser.parse()
 
-    # Write the results to the output file
-    write_results_to_file(data, output_file)
+    initial_sol = InitialSolution.generate_initial_solution(input_file)
+    initial_sol.write_results()
+
+    # # Write the results to the output file
+    # write_results_to_file(data, output_file)
